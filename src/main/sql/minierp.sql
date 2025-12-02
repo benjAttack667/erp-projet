3c938242ea4a5e4a4f5ddd984378feafbd4ab103
Thierry <thyresias@gmail.com>
2025-12-02 16:53:18 +0100

    base de données id String

--- src/main/sql/minierp.sql
+++ src/main/sql/minierp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 02 déc. 2025 à 13:41
+-- Généré le : mar. 02 déc. 2025 à 15:50
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.3.1
 
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `taskId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `content` text NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+                            `id` varchar(255) NOT NULL,
+                            `taskId` int(11) NOT NULL,
+                            `userId` int(11) NOT NULL,
+                            `content` text NOT NULL,
+                            `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -42,12 +42,12 @@ CREATE TABLE `comments` (
 --
 
 CREATE TABLE `projects` (
-  `id` int(11) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `description` text,
-  `managerId` int(11) NOT NULL,
-  `startDate` date DEFAULT NULL,
-  `endDate` date DEFAULT NULL
+                            `id` varchar(255) NOT NULL,
+                            `name` varchar(150) NOT NULL,
+                            `description` text,
+                            `managerId` int(11) NOT NULL,
+                            `startDate` date DEFAULT NULL,
+                            `endDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -57,13 +57,13 @@ CREATE TABLE `projects` (
 --
 
 CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `title` varchar(150) NOT NULL,
-  `description` text,
-  `status` varchar(50) NOT NULL,
-  `dueDate` date DEFAULT NULL,
-  `projectId` int(11) NOT NULL,
-  `assignedTo` int(11) NOT NULL
+                         `id` varchar(255) NOT NULL,
+                         `title` varchar(150) NOT NULL,
+                         `description` text,
+                         `status` varchar(50) NOT NULL,
+                         `dueDate` date DEFAULT NULL,
+                         `projectId` int(11) NOT NULL,
+                         `assignedTo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -73,13 +73,13 @@ CREATE TABLE `tasks` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `surname` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `passwordHash` varchar(255) NOT NULL,
-  `role` varchar(50) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `dateOfBirth` date DEFAULT NULL
+                         `id` varchar(255) NOT NULL,
+                         `surname` varchar(100) NOT NULL,
+                         `name` varchar(100) NOT NULL,
+                         `passwordHash` varchar(255) NOT NULL,
+                         `role` varchar(50) NOT NULL,
+                         `email` varchar(150) NOT NULL,
+                         `dateOfBirth` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -90,54 +90,26 @@ CREATE TABLE `users` (
 -- Index pour la table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `projects`
 --
 ALTER TABLE `projects`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `projects`
---
-ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
