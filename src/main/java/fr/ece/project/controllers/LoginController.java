5589114fac672cb832b42ae4f9ec994e826def71
Thierry <thyresias@gmail.com>
2025-12-03 11:19:04 +0100

    login page

--- src/main/java/fr/ece/project/controllers/LoginController.java
+++ src/main/java/fr/ece/project/controllers/LoginController.java
@@ -6,14 +6,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML private TextField usernameField;
+    @FXML private TextField emailField;
     @FXML private TextField passwordField;
 
     private final AuthService authService = new AuthService();
 
     @FXML
     public void onLogin(ActionEvent e) {
-        boolean ok = authService.login(usernameField.getText(), passwordField.getText());
+        boolean ok = authService.login(emailField.getText(), passwordField.getText());
         if (ok) {
             System.out.println("Connexion réussie !");
         } else {
